5b75800276d4406baf70ec608a3c1a16b6f55c50
Alex Dergachev <alex@evolvingweb.ca>
2012-10-15 18:19:46 -0400

    Updated metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,11 @@
-maintainer       "Oversun-Scalaxy LTD"
-maintainer_email "d.lebedev@scalaxy.ru"
+maintainer       "Alex Dergachev"
+maintainer_email "alex@evolvingweb.ca"
 license          "Apache 2.0"
 description      "Installs/Configures redmine"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-depends          "rvm"
+
+# depends          "rvm"
+# depends          "mysql"
+# depends          "database"
+# depends          "nginx"
