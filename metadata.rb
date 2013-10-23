2c72db8c52a7b47481f1f002d1121fa62fdd7a94
Alex Dergachev <alex@evolvingweb.ca>
2013-10-23 15:29:25 -0400

    Added explicit cookbook dependencies to metadata.rb; prevents loading errors refs #1

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description      "Installs/Configures redmine"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-# depends          "rvm"
-# depends          "mysql"
-# depends          "database"
-# depends          "nginx"
+depends          "mysql"
+depends          "database"
+depends          "nginx"
+depends          "apt"
