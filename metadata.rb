b6369b10f508ecd61a2e7a409eba8a3bbd2c3d8f
Alex Dergachev <alex@evolvingweb.ca>
2013-10-23 17:01:45 -0400

    Tagging v0.0.2

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email "alex@evolvingweb.ca"
 license          "Apache 2.0"
 description      "Installs/Configures redmine"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 depends          "mysql"
 depends          "database"
